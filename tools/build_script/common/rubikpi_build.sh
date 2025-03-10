c494ee96505bd33ebb640e7a204707d1c903ed3a
hongyang-rp <hongyang.zhao@thundersoft.com>
2025-03-10 20:42:09 +0800

    first commit

---
+++ tools/build_script/common/rubikpi_build.sh
@@ -0,0 +1,308 @@
+#!/bin/sh
+# Author: Zhao Hongyang <hongyang.zhao@thundersoft.com>
+# Date: 2024-11-15
+# Copyright (c) 2024, Thundercomm All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+set -e
+
+BOOTIMG_EXTRA_SPACE="512"
+BOOTIMG_VOLUME_ID="BOOT"
+TOP_DIR=`pwd`
+KERNEL_CONFIG="qcom_defconfig"
+CONFIG_FILE_DIR="${TOP_DIR}/arch/arm64/configs"
+KERNEL_CONFIG_FRAGMENTS="${CONFIG_FILE_DIR}/qcom_addons.config \
+						 ${CONFIG_FILE_DIR}/selinux.config \
+						 ${CONFIG_FILE_DIR}/rubikpi3.config"
+
+usage() {
+	echo "\033[1;37mUsage:\033[0m"
+	echo "  bash $0 [options]"
+	echo
+	echo "\033[1;37mOptions:\033[0m"
+	echo "\033[1;37m  -h, --help\033[0m              display this help message"
+	echo "\033[1;37m  -dp, --dtb_package\033[0m      generate a burnable device tree image"
+	echo "\033[1;37m  -ip, --image_package\033[0m    generate a burnable kernel image"
+	echo "\033[1;37m  -a, --build_all\033[0m         Complete compilation of kernel"
+	echo "\033[1;37m  -d, --build_dts\033[0m         Complete compilation of device tree"
+	echo "\033[1;37m  -gc, --generate_config\033[0m  Generate. config file"
+	echo "\033[1;37m  -c, --clean\033[0m             Clean up workspace"
+	echo
+}
+
+build_fat_img() {
+	FATSOURCEDIR=$1
+	FATIMG=$2
+
+	echo $FATSOURCEDIR $FATIMG
+
+	# Calculate the size required for the final image including the
+	# data and filesystem overhead.
+	# Sectors: 512 bytes
+	# Blocks: 1024 bytes
+
+	# Determine the sector count just for the data
+	SECTORS=$(expr $(du --apparent-size -ks ${FATSOURCEDIR} | cut -f 1) \* 2)
+
+	# Account for the filesystem overhead.
+	# 32 bytes per dir entry
+	DIR_BYTES=$(expr $(find ${FATSOURCEDIR} | tail -n +2 | wc -l) \* 32)
+	# 32 bytes for every end-of-directory dir entry
+	DIR_BYTES=$(expr $DIR_BYTES + $(expr $(find ${FATSOURCEDIR} -type d | tail -n +2 | wc -l) \* 32))
+	# 4 bytes per FAT entry per sector of data
+	FAT_BYTES=$(expr $SECTORS \* 4)
+	# 4 bytes per FAT entry per end-of-cluster list
+	FAT_BYTES=$(expr $FAT_BYTES + $(expr $(find ${FATSOURCEDIR} -type d | tail -n +2 | wc -l) \* 4))
+
+	# Use a ceiling function to determine FS overhead in sectors
+	DIR_SECTORS=$(expr $(expr $DIR_BYTES + 511) / 512)
+	# There are two FATs on the image
+	FAT_SECTORS=$(expr $(expr $(expr $FAT_BYTES + 511) / 512) \* 2)
+	SECTORS=$(expr $SECTORS + $(expr $DIR_SECTORS + $FAT_SECTORS))
+
+	# Determine the final size in blocks accounting for some padding
+	BLOCKS=$(expr $(expr $SECTORS / 2) + ${BOOTIMG_EXTRA_SPACE})
+
+	# mkfs.vfat will sometimes use FAT16 when it is not appropriate,
+	# resulting in a boot failure. Use FAT32 for images larger
+	# than 512MB, otherwise let mkfs.vfat decide.
+	if [ $(expr $BLOCKS / 1024) -gt 512 ]; then
+		FATSIZE="-F 32"
+	fi
+
+	# Delete any previous image.
+	if [ -e ${FATIMG} ]; then
+		rm ${FATIMG} -f
+	fi
+
+	mkfs.vfat ${FATSIZE} -n ${BOOTIMG_VOLUME_ID} ${MKFSVFAT_EXTRAOPTS} -C ${FATIMG} ${BLOCKS}
+
+	# Copy FATSOURCEDIR recursively into the image file directly
+	mcopy -i ${FATIMG} -s ${FATSOURCEDIR}/* ::/
+}
+
+do_dtb_package()
+{
+	mkdir -p ${TOP_DIR}/rubikpi/tools/pack/dtb_temp/dtb
+
+	mkdir -p ${TOP_DIR}/rubikpi/output/pack
+
+	cp ${TOP_DIR}/arch/arm64/boot/dts/qcom/rubikpi3-6490.dtb ${TOP_DIR}/rubikpi/tools/pack/dtb_temp/dtb/combined-dtb.dtb
+
+	build_fat_img ${TOP_DIR}/rubikpi/tools/pack/dtb_temp/dtb ${TOP_DIR}/rubikpi/output/pack/dtb.bin
+
+	rm ${TOP_DIR}/rubikpi/tools/pack/dtb_temp -rf
+}
+
+# DEFAULT_CMDLINE="initrd=\ostree\poky-960324eb402e612cd2ddd5c3b64db972337059114f4354eb95976f59a0ec3922\initramfs-6.6.52-qli-1.3-ver.1.1.img \
+# 				root=/dev/disk/by-partlabel/system rw rootwait \
+# 				console=ttyMSM0,115200n8 earlycon qcom_geni_serial.con_enabled=1 \
+# 				kernel.sched_pelt_multiplier=4 \
+# 				mem_sleep_default=s2idle"
+
+DEFAULT_CMDLINE="root=/dev/disk/by-partlabel/system rw rootwait console=ttyMSM0,115200n8 earlycon qcom_geni_serial.con_enabled=1 kernel.sched_pelt_multiplier=4 mem_sleep_default=s2idle"
+
+
+do_image_package()
+{
+	mkdir -p ${TOP_DIR}/rubikpi/tools/pack/image_temp
+	mkdir -p ${TOP_DIR}/rubikpi/tools/pack/image_temp/mnt
+	mkdir -p ${TOP_DIR}/rubikpi/output/pack
+
+	cp ${TOP_DIR}/arch/arm64/boot/Image ${TOP_DIR}/rubikpi/tools/pack/image_temp
+
+	sudo python3.10 ${TOP_DIR}/rubikpi/tools/pack/ukify build \
+		--efi-arch=aa64 \
+		--stub="${TOP_DIR}/rubikpi/tools/pack/linuxaa64.efi.stub" \
+		--linux="${TOP_DIR}/rubikpi/tools/pack/image_temp/Image" \
+		--initrd="${TOP_DIR}/rubikpi/tools/pack/initramfs-ostree-image-qcm6490-idp.cpio.gz" \
+		--cmdline="${DEFAULT_CMDLINE}" \
+		--output="${TOP_DIR}/rubikpi/tools/pack/image_temp/linux-qcm6490-idp.efi"
+
+	cp ${TOP_DIR}/rubikpi/tools/pack/efi.bin ${TOP_DIR}/rubikpi/tools/pack/image_temp
+	sudo mount ${TOP_DIR}/rubikpi/tools/pack/image_temp/efi.bin ${TOP_DIR}/rubikpi/tools/pack/image_temp/mnt --options rw
+
+	sudo cp ${TOP_DIR}/rubikpi/tools/pack/image_temp/linux-qcm6490-idp.efi ${TOP_DIR}/rubikpi/tools/pack/image_temp/mnt/ostree/poky-*/vmlinuz-6.6.52
+	sudo umount ${TOP_DIR}/rubikpi/tools/pack/image_temp/mnt
+	sudo cp ${TOP_DIR}/rubikpi/tools/pack/image_temp/efi.bin ${TOP_DIR}/rubikpi/output/pack
+
+	rm ${TOP_DIR}/rubikpi/tools/pack/image_temp -rf
+}
+
+kernel_conf_variable() {
+	sed -e "/CONFIG_$1[ =]/d;" -i ${TOP_DIR}/.config
+	if test "$2" = "n"
+	then
+		echo "# CONFIG_$1 is not set" >> ${TOP_DIR}/.config
+	else
+		echo "CONFIG_$1=$2" >> ${TOP_DIR}/.config
+	fi
+}
+
+do_merge_config()
+{
+	cp ${CONFIG_FILE_DIR}/${KERNEL_CONFIG} .config
+
+	kernel_conf_variable LOCALVERSION "\"\""
+	kernel_conf_variable LOCALVERSION_AUTO n
+
+	scripts/kconfig/merge_config.sh -m -r -O ${TOP_DIR} ${TOP_DIR}/.config ${KERNEL_CONFIG_FRAGMENTS} 1>&2
+
+	mv .config ${CONFIG_FILE_DIR}/rubikpi3_defconfig
+	make ARCH=arm64 rubikpi3_defconfig
+
+	rm ${CONFIG_FILE_DIR}/rubikpi3_defconfig
+}
+
+do_build_all()
+{
+	if [ ! -e "${TOP_DIR}/.config" ]; then
+		do_merge_config
+	fi
+
+	make ARCH=arm64 CROSS_COMPILE=aarch64-qcom-linux- LOCALVERSION="" dir-pkg INSTALL_MOD_STRIP=1 -j`nproc`
+}
+
+do_build_dtb()
+{
+	if [ ! -e "${TOP_DIR}/.config" ]; then
+		do_merge_config
+	fi
+
+	make ARCH=arm64 CROSS_COMPILE=aarch64-qcom-linux- LOCALVERSION="" -j`nproc` qcom/rubikpi3-6490.dtb
+}
+
+do_clean()
+{
+	make clean
+	if [ -e .config ]; then
+		rm .config
+	fi
+}
+
+do_before_compilation()
+{
+	if [ ! -e ${TOP_DIR}/rubikpi/.bc_config_done ]; then
+		cp -n ${TOP_DIR}/rubikpi/tools/pack/config/selinux.config \
+			${TOP_DIR}/arch/arm64/configs/selinux.config
+
+		cp -n ${TOP_DIR}/rubikpi/tools/pack/dts/* \
+			${TOP_DIR}/arch/arm64/boot/dts/qcom
+	
+		cp -n ${TOP_DIR}/arch/arm64/boot/dts/thundercomm/rubikpi3/rubikpi3* \
+			${TOP_DIR}/arch/arm64/boot/dts/qcom
+
+		cp -n ${TOP_DIR}/rubikpi/tools/pack/include/msm-camera.h \
+				${TOP_DIR}/include/dt-bindings
+
+		cp -n ${TOP_DIR}/rubikpi/tools/pack/makefile/Makefile \
+				${TOP_DIR}/techpack/
+
+		sed -i '/DBUILD_TIMESTAMP/c\# CDEFINES += -DBUILD_TIMESTAMP=\"$(shell date -u +'%Y-%m-%dT%H:%M:%SZ')\"' \
+			${TOP_DIR}/techpack/display/msm/Kbuild
+
+		sed -i "99iobj-y			+= techpack/" Kbuild
+
+		target_file=${TOP_DIR}/arch/arm64/boot/dts/qcom/Makefile
+		sed -i '2iifdef RUBIKPI_DTB_BUILD_ALL' ${target_file}
+		marker_line=$(grep -n '# rubikpi' "${target_file}" | cut -d: -f1)
+		if [ -z "$marker_line" ]; then
+			echo "error: no '# rubikpi' tag found"
+			exit 1
+		fi
+		insert_line=$((marker_line - 1))
+		sed -i "${insert_line}iendif" "${target_file}"
+
+		sed -i '/# rubikpi/,$d' "${target_file}"
+		cat <<'EOF' >> "$target_file"
+# rubikpi
+dtb-$(CONFIG_ARCH_QCOM)	+= rubikpi3-6490.dtb
+rubikpi3-6490-dtbs := \
+	rubikpi3.dtb qcm6490-graphics.dtbo qcm6490-display.dtbo \
+	qcm6490-camera-idp.dtbo qcm6490-video.dtbo rubikpi3-overlay.dtbo
+EOF
+		touch ${TOP_DIR}/rubikpi/.bc_config_done
+	fi
+}
+
+# ========================== Start ========================================
+do_dtb_package_flag=0
+do_image_package_flag=0
+do_build_all_flag=0
+do_build_dtb_flag=0
+do_clean_flag=0
+do_merge_config_flag=0
+
+do_before_compilation
+if [ "$1" != "" ]; then
+	while true; do
+		case "$1" in
+			-h|--help)              usage; exit 0;;
+			-dp|--dtb_package)      do_dtb_package_flag=1 ;;
+			-ip|--image_package)    do_image_package_flag=1 ;;
+			-a|--build_all)         do_build_all_flag=1 ;;
+			-d|--build_dts)         do_build_dtb_flag=1 ;;
+			-gc|--generate_config)   do_merge_config_flag=1 ;;
+			-c|--clean)   do_clean_flag=1 ;;
+		esac
+		shift
+
+		if [ "$1" = "" ]; then
+			break
+		fi
+	done
+
+	if [ "$do_clean_flag" -eq "1" ]; then
+		do_clean
+	fi
+
+	if [ "$do_merge_config_flag" -eq "1" ]; then
+		do_merge_config
+	fi
+
+	if [ ${do_build_all_flag} -eq "1" ]; then
+		do_build_all
+	fi
+
+	if [ ${do_build_dtb_flag} -eq "1" ]; then
+		do_build_dtb
+	fi
+
+	if [ ${do_dtb_package_flag} -eq "1" ]; then
+		do_dtb_package
+	fi
+
+	if [ ${do_image_package_flag} -eq "1" ]; then
+		do_image_package
+	fi
+else
+	do_build_all
+	do_dtb_package
+	do_image_package
+fi
+
---
+++ tools/build_script/common/rubikpi_flash.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+# Author: Zhao Hongyang <hongyang.zhao@thundersoft.com>
+# Date: 2024-11-15
+# Copyright (c) 2024, Thundercomm All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+set -e
+
+TOP_DIR=`pwd`
+
+usage() {
+	echo "\033[1;37mUsage:\033[0m"
+	echo "  bash $0 [options]"
+	echo
+	echo "\033[1;37mOptions:\033[0m"
+	echo "\033[1;37m  -h, --help\033[0m     display this help message"
+	echo "\033[1;37m  -r, --reboot\033[0m   exit fastboot mode"
+	echo "\033[1;37m  -d, --dtb\033[0m      flash device tree image"
+	echo "\033[1;37m  -i, --image\033[0m    flash kernel image"
+	echo
+}
+
+do_dtb_flash() {
+	if [ ! -f "$TOP_DIR/rubikpi/output/pack/dtb.bin" ]; then
+		echo "No dtb.bin file found, please use rubikpi-build. sh to generate"
+		exit
+	fi
+
+	fastboot flash dtb_a $TOP_DIR/rubikpi/output/pack/dtb.bin
+}
+
+do_image_flash() {
+	if [ ! -f "$TOP_DIR/rubikpi/output/pack/efi.bin" ]; then
+		echo "No efi.bin file found, please use rubikpi-build. sh to generate"
+		exit
+	fi
+
+	fastboot flash efi $TOP_DIR/rubikpi/output/pack/efi.bin
+}
+
+do_fastboot_reboot() {
+	fastboot reboot
+}
+
+# ========================== Start ========================================
+do_dtb_flash_flag=0
+do_image_flash_flag=0
+do_fastboot_reboot_flag=0
+
+if [ "$1" != "" ]; then
+	while true; do
+		case "$1" in
+			-h|--help)           usage; exit 0;;
+			-d|--dtb)            do_dtb_flash_flag=1 ;;
+			-i|--image)          do_image_flash_flag=1 ;;
+			-r|--reboot)         do_fastboot_reboot_flag=1 ;;
+		esac
+		shift
+
+		if [ "$1" = "" ]; then
+			break
+		fi
+	done
+
+	if [ "$do_dtb_flash_flag" -eq "1" ]; then
+		do_dtb_flash
+	fi
+
+	if [ "$do_image_flash_flag" -eq "1" ]; then
+		do_image_flash
+	fi
+
+	if [ "$do_fastboot_reboot_flag" -eq "1" ]; then
+		do_fastboot_reboot
+	fi
+else
+	do_dtb_flash
+	do_image_flash
+	do_fastboot_reboot
+fi
+
